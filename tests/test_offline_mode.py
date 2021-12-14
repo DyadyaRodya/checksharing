2675af1e7ddcae5e78d4c0d6ea7aff5fe3d0ad61
aglsrz <sirazovaaigul@gmail.com>
2021-12-14 19:21:47 +0300

    Edit tests for bill

--- tests/test_offline_mode.py
+++ tests/test_offline_mode.py
@@ -13,31 +13,26 @@ class TestOfflineRoom(unittest.TestCase):
         self.rc.add_member(name="Rob", global_id=300)
 
     def test_local_room_bills(self):
-        self.rc.add_member(name="Mary", global_id=200)
-        self.rc.add_bill_to_room("local", self.m_id, 1, "Claude Monet", 20000)
-        total_sum = self.rc.add_bill_to_room("local", 200, 2, "Claude Monet", 20000)
-        self.assertEqual(total_sum, 40000)
+        b_id1 = self.rc.add_bill_to_room("local", self.m_id, "Claude Monet", 20000)
+        b_id2 = self.rc.add_bill_to_room("local", 300, "Claude Monet", 20000)
+        self.assertIsInstance(b_id2, UUID)
+        self.assertEqual(self.rc.get_room_info("local").get_total_summ(), 40000)
 
-        total_sum = self.rc.edit_bill_room("local", 2, 200, 3, "Claude Monet", 30000)
-        self.assertIsNone(self.rc.add_bill_to_room("local", 200, 4, "Claude Monet", 70000))
-        self.assertIsNone(self.rc.add_bill_to_room("newroom", 200, 4, "Claude Monet", 1000))
-        self.assertIsNone(self.rc.add_bill_to_room("local", 555, 4, "Claude Monet", 1000))
-        self.assertEqual(total_sum, 50000)
+        edit_id = self.rc.edit_bill_room("local", b_id2, 300, "Claude Monet", 30000)
+        self.assertEqual(edit_id, b_id2)
+        self.assertIsNone(self.rc.add_bill_to_room("local", 300, "Claude Monet", 70000))
+        self.assertIsNone(self.rc.add_bill_to_room("newroom", 100, "Claude Monet", 1000))
+        self.assertIsNone(self.rc.add_bill_to_room("local", 555, "Claude Monet", 1000))
+        self.assertEqual(self.rc.get_room_info("local").get_total_summ(), 50000)
 
-        self.assertIsNone(self.rc.del_bill_from_room("local", 2))
-        self.assertIsNone(self.rc.del_bill_from_room("newroom", 3))
-        total_sum = self.rc.del_bill_from_room("local", 3)
-        self.assertEqual(total_sum, 20000)
+        self.assertIsNone(self.rc.del_bill_from_room("local", 132))
+        self.assertIsNone(self.rc.del_bill_from_room("newroom", b_id2))
+        self.rc.del_bill_from_room("local", b_id2)
+        self.assertEqual(self.rc.get_room_info("local").get_total_summ(), 20000)
 
-        winners = self.rc.get_winners_in_the_room()
-        self.assertIsInstance(winners, list)
-        print(winners)
-
-
-class TestMembersListEdit(unittest.TestCase):
-    def setUp(self):
-        self.rc = RoomController()
-        self.m_id = self.rc.add_member("John")
+        self.rc.set_winners_in_the_room()
+        winners_l = self.rc.get_winners_in_the_room()
+        self.assertIsInstance(winners_l, list)
 
     def test_get_room(self):
         room = self.rc.get_room_by_name('local')
@@ -45,9 +40,7 @@ class TestMembersListEdit(unittest.TestCase):
         self.assertEqual(room.get_name(), 'local')
 
     def test_members_list_edit(self):
-        # m_id = self.rc.add_member("John")
         self.assertIsInstance(self.m_id, UUID)
-        # self.assertFalse(self.rc.add_member(name="John", global_id=self.m_id))
         m2_id = self.rc.add_member(name="Mary", global_id=self.m_id)
         self.assertFalse(m2_id)
         m2_id = self.rc.add_member(name="John", room_name="second_room")
