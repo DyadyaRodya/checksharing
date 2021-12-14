77a0c58f99bf0ce094e364824502a6fa01c34195
aglsrz <sirazovaaigul@gmail.com>
2021-12-14 19:36:26 +0300

    Edit tests for bill

--- tests/test_offline_mode.py
+++ tests/test_offline_mode.py
@@ -13,14 +13,15 @@ class TestOfflineRoom(unittest.TestCase):
         self.rc.add_member(name="Rob", global_id=300)
 
     def test_local_room_bills(self):
-        b_id1 = self.rc.add_bill_to_room("local", self.m_id, "Claude Monet", 20000)
+        b_id1 = self.rc.add_bill_to_room("local", self.m_id, "Claude Monet", 20000, bill_id=1)
         b_id2 = self.rc.add_bill_to_room("local", 300, "Claude Monet", 20000)
-        self.assertIsInstance(b_id2, UUID)
+        self.assertEqual(b_id1, 1)
         self.assertEqual(self.rc.get_room_info("local").get_total_summ(), 40000)
 
         edit_id = self.rc.edit_bill_room("local", b_id2, 300, "Claude Monet", 30000)
         self.assertEqual(edit_id, b_id2)
         self.assertIsNone(self.rc.add_bill_to_room("local", 300, "Claude Monet", 70000))
+        self.assertFalse(self.rc.add_bill_to_room("local", 300, "Claude Monet", 7000, bill_id=1))
         self.assertIsNone(self.rc.add_bill_to_room("newroom", 100, "Claude Monet", 1000))
         self.assertIsNone(self.rc.add_bill_to_room("local", 555, "Claude Monet", 1000))
         self.assertEqual(self.rc.get_room_info("local").get_total_summ(), 50000)
@@ -48,6 +49,19 @@ class TestOfflineRoom(unittest.TestCase):
 
     def test_delete_member(self):
         self.assertTrue(self.rc.del_member(global_id=self.m_id))
+
+    def test_reset_local_room(self):
+        self.assertEqual(self.rc.reset_local_room(), "local")
+
+    def test_start_delete_new_room(self):
+        self.assertIsNone(self.rc.start_new_room("local"))
+        self.assertEqual(self.rc.start_new_room("new_room"), "new_room")
+        self.assertTrue(self.rc.delete_room("new_room"))
+
+    def test_edit_payment_settings(self):
+        m2_id = self.rc.add_member(name="Mary")
+        self.assertIsNone(self.rc.edit_payment_settings(10))
+        self.assertIsNone(self.rc.edit_payment_settings(1))
 
 
 if __name__ == "__main__":
